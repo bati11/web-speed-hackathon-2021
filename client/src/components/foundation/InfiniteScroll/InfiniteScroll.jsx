3dcf7fcf0d92b5b7ae47ae235e4a67773efa8f77
bati11 <mail.bati11@gmail.com>
2022-01-01 17:14:53 +0900

    optimize checking hasReached

--- client/src/components/foundation/InfiniteScroll/InfiniteScroll.jsx
+++ client/src/components/foundation/InfiniteScroll/InfiniteScroll.jsx
@@ -9,16 +9,32 @@ import React from 'react';
 
 /** @type {React.VFC<Props>} */
 const InfiniteScroll = ({ children, fetchMore, items }) => {
+  const [processing, setProcessing] = React.useState(false);
+
   const latestItem = items[items.length - 1];
 
   const prevReachedRef = React.useRef(false);
 
   React.useEffect(() => {
     const handler = () => {
+      if (processing) {
+        return true
+      }
+      setProcessing(true)
       // 念の為 2の18乗 回、最下部かどうかを確認する
-      const hasReached = Array.from(Array(2 ** 18), () => {
-        return window.innerHeight + Math.ceil(window.scrollY) >= document.body.offsetHeight;
-      }).every(Boolean);
+      // const hasReached = Array.from(Array(2 ** 18), () => {
+      //   return window.innerHeight + Math.ceil(window.scrollY) >= document.body.offsetHeight;
+      // }).every(Boolean);
+
+      // let hasReached = false
+      // for (let i = 0; i < 2 ** 18; i++) {
+      //   hasReached = window.innerHeight + Math.ceil(window.scrollY) >= document.body.offsetHeight;
+      //   if (!hasReached) {
+      //     return
+      //   }
+      // }
+
+      const hasReached = window.innerHeight + Math.ceil(window.scrollY) >= document.body.offsetHeight;
 
       // 画面最下部にスクロールしたタイミングで、登録したハンドラを呼び出す
       if (hasReached && !prevReachedRef.current) {
@@ -29,6 +45,7 @@ const InfiniteScroll = ({ children, fetchMore, items }) => {
       }
 
       prevReachedRef.current = hasReached;
+      setProcessing(false)
     };
 
     // 最初は実行されないので手動で呼び出す
