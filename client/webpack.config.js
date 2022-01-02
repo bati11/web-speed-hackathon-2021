d85a2d3762bf75138cfc47b2f46d440de26b9d9b
bati11 <mail.bati11@gmail.com>
2022-01-03 00:09:50 +0900

    optimize css file size

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const webpack = require('webpack');
 
 const SRC_PATH = path.resolve(__dirname, './src');
@@ -74,6 +75,7 @@ const config = {
     minimize: true,
     minimizer: [
       new TerserPlugin(),
+      new CssMinimizerPlugin(),
     ],
   },
   resolve: {
