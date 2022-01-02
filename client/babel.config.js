6656c6d2a9795038d7fb9475e79ad39d5409f9e6
bati11 <mail.bati11@gmail.com>
2022-01-02 21:32:00 +0900

    add preset-env

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   presets: [
     [
+      "@babel/preset-env",
+      {
+        "targets": "last 2 versions"
+      },
+    ],
+    [
       '@babel/preset-react',
       {
         development: false,
